4e1fa6381235fcb776c3da12f31ba09c7b69d7f8
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-02-12 18:21:46 +0300

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/app/App';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('roo'));
 root.render(
   <BrowserRouter>
     <App/>
